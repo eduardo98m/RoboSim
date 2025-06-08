f831f0efd68a590cbca293de6b874b2fd7668480
eduardo98m <eduardo98m@gmail.com>
2025-06-08 23:58:24 +0900

    deleted comments

--- src/physicsAcc/API/World.hpp
+++ src/physicsAcc/API/World.hpp
@@ -76,7 +76,6 @@ struct World
         scalar h = timestep / this->substeps;
         scalar inv_h = 1 / h;
 
-        // this->broad_phase_collision_detection();
         update_collider_poses(colliders, bodies, timestep);
         BroadPhaseResult broad_phase_pairs = broad_phase_collision_detection(colliders);
 
